a2e43b9ae0defed6e136e196d6f8593c904ede70
ejain077 <ejain077@PR25.hhs.win.fuhsd.org>
2018-05-16 11:26:55 -0700

    added menu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,40 @@
+import java.awt.CardLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import processing.awt.PSurfaceAWT;
 
-public class Main {
+public class Main extends JFrame {
+	
+	private JPanel cardPanel;
+	
+	public Main(String title) {
+		super(title);
+		setBounds(100, 100, 800, 600);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    cardPanel = new JPanel();
+	    CardLayout cl = new CardLayout();
+	    cardPanel.setLayout(cl);
+	    
+		OptionPanel panel1 = new OptionPanel(this);    
+	    GamePanel panel2 = new GamePanel(this);
+	
+	    cardPanel.add(panel1,"1"); // Card is named "1"
+	    cardPanel.add(panel2,"2"); // Card is named "2"
+	    
+	    add(cardPanel);
+	    addKeyListener(panel2);
+	
+	    setVisible(true);
+	}
+
 	public static void main(String args[]) {
+		
+		Main w = new Main("AP Animation Demo");
+
 		DrawingSurface drawing = new DrawingSurface();
 
 		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
@@ -18,5 +47,11 @@ public class Main {
 		window.setResizable(true);
 
 		window.setVisible(true);
+	}
+	
+	
+	public void changePanel(String name) {
+		((CardLayout)cardPanel.getLayout()).show(cardPanel,name);
+		requestFocus();
 	}
 }
