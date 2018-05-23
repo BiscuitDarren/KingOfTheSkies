93e59be8ed24f262c57870f08677517fc06cfd42
darren <dbiskup559@student.fuhsd.org>
2018-05-23 00:29:37 -0700

    some bugfixes

--- src/BackgroundController.java
+++ src/BackgroundController.java
@@ -34,42 +34,44 @@ public class BackgroundController {
 	}
 
 	public void draw(PApplet p) {
-		// Loop through the PApplet's pixel array
-		if (bg.width != p.width || bg.height != p.height) {
-			bg.resize(p.width, p.height);
-			bg.loadPixels();
-		}
-		update(p.width, p.height);
-		p.loadPixels();
+		try {
+			// Loop through the PApplet's pixel array
+			if (bg.width != p.width || bg.height != p.height) {
+				bg.resize(p.width, p.height);
+				bg.loadPixels();
+			}
+			update(p.width, p.height);
+			p.loadPixels();
 
-		if (p.pixels.length != bg.pixels.length)
-			System.out.println("ERROR! PIXEL ARRAY NOT SAME LENGTH");
-		
-		for (int i = 0; i < bg.pixels.length - 1; i++) {
-			int bgx = actualX + i % bg.width;
-			int bgy = actualY + i / bg.width;
+			if (p.pixels.length != bg.pixels.length)
+				System.out.println("ERROR! PIXEL ARRAY NOT SAME LENGTH");
 
-			int px = 0, py = 0;
-			if (bgx > 0)
-				px = bgx % p.width;
-			else if (bgx < 0)
-				px = p.width + bgx;
+			for (int i = 0; i < bg.pixels.length - 1; i++) {
+				int bgx = actualX + i % bg.width;
+				int bgy = actualY + i / bg.width;
 
-			if (bgy > 0)
-				py = bgy % p.height;
-			else if (bgy < 0)
-				py = p.height + bgy;
+				int px = 0, py = 0;
+				if (bgx > 0)
+					px = bgx % p.width;
+				else if (bgx < 0)
+					px = p.width + bgx;
 
-			// Finally, set the pixels to be correctly equal
-			int pval = py * p.width + px;
-			int bgval = i / bg.width * bg.width + i % bg.width;
-	
+				if (bgy > 0)
+					py = bgy % p.height;
+				else if (bgy < 0)
+					py = p.height + bgy;
+
+				// Finally, set the pixels to be correctly equal
+				int pval = py * p.width + px;
+				int bgval = i / bg.width * bg.width + i % bg.width;
+
 				p.pixels[py * p.width + px] = bg.pixels[i / bg.width * bg.width + i % bg.width];
-			
-		}
 
-		// Real finally, update the PApplet's pixels
-		p.updatePixels();
+			}
+			p.updatePixels();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return;
+		}
 	}
 
 	public void resetPlayer(PMovingImage player) {
